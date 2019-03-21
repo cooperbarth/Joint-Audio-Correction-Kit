a308c847d3b266c57a3f60df250ac5de747aced7
Jack Wiig <jhwiig@gmail.com>
2019-03-21 01:33:40 -0500

    Added Try/Except

--- Noise_Cancellation/cancel.py
+++ Noise_Cancellation/cancel.py
@@ -1,6 +1,6 @@
-'''PyAudio use inspired by:
+"""PyAudio use inspired by:
     https://gist.github.com/aflaxman/6300595
-'''
+"""
 
 import numpy as np, scipy.signal, pyaudio
 
@@ -12,7 +12,7 @@ DEFAULT_SR = 44100
 CHUNK = 2048
 DTYPE = np.int16
 
-#Create stream
+# Create stream
 p = pyaudio.PyAudio()
 stream = p.open(format=p.get_format_from_width(WIDTH),
                 channels=NUM_CHANNELS,
@@ -21,35 +21,41 @@ stream = p.open(format=p.get_format_from_width(WIDTH),
                 output=True,
                 frames_per_buffer=CHUNK)
 
-noise_filter = np.fft.fft(np.random.rand(2*CHUNK))
+# noise_filter = np.fft.fft(np.random.rand(2*CHUNK))
 filter_count = 0
 
-#every loop, update buffer and multiply input by inverse of buffer
-while True:
-    #getting fft of current input chunk
-    audio_input = np.frombuffer(stream.read(CHUNK), dtype=DTYPE)
-    MAX_INT = max(audio_input)
-    if MAX_INT == 0:
-        continue
-    normalized_audio_input = audio_input / MAX_INT
-    freq_input = np.fft.fft(normalized_audio_input)
+# have an array of 1000 noise filters
+noise_filter = np.ndarray((2*CHUNK, 1000), dtype=DTYPE)
 
-    #update buffer with weighted average
-    filter_weighted = filter_count * noise_filter
-    filter_count += 1
+try:
+    # every loop, update buffer and multiply input by inverse of buffer
+    while True:
+        # getting fft of current input chunk
+        audio_input = np.frombuffer(stream.read(CHUNK), dtype=DTYPE)
+        MAX_INT = max(audio_input)
+        if MAX_INT == 0:
+            continue
+        normalized_audio_input = audio_input / MAX_INT
+        freq_input = np.fft.fft(normalized_audio_input)
 
-    noise_filter = (filter_weighted + freq_input) / filter_count
+        # update buffer with weighted average
+        filter_weighted = filter_count * noise_filter
+        filter_count += 1
 
-    filter_inverse = -1 * noise_filter
+        noise_filter = (filter_weighted + freq_input) / filter_count
 
-    #multiplier should be inverse of buffer
-    freq_generated = freq_input * filter_inverse
-    cancel_sound = np.real(np.fft.ifft(freq_generated))
+        filter_inverse = -1 * noise_filter
 
-    #output sound
-    write_data = np.array(np.round(cancel_sound * MAX_INT), dtype=DTYPE)
-    stream.write(write_data.tostring(), CHUNK)
+        # multiplier should be inverse of buffer
+        freq_generated = freq_input * filter_inverse
+        cancel_sound = np.real(np.fft.ifft(freq_generated))
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+        # output sound
+        write_data = np.array(np.round(cancel_sound * MAX_INT), dtype=DTYPE)
+        stream.write(write_data.tostring(), CHUNK)
+
+except:
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
